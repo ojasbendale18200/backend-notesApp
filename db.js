1974bbfba97a217631402d93b4894a10ca7b27e6
Ojas <ojasbendale18200@gmail.com>
2023-03-25 18:05:45 +0530

    env

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// dasda
+
 const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = { connection };
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { auth } = require("./middlewares/auth.middleware");
 const { noteRouter } = require("./routes/notes.routes");
 const { userRouter } = require("./routes/user.routes");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
@@ -23,7 +24,7 @@ app.listen(4000, async () => {
     await connection;
     console.log("connected to DB");
   } catch (error) {
-    console.log("Not Connected");
+    console.log(error);
   }
 
   console.log("server running on port 4000");
